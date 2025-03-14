a0101429f23f45339ed12982a998aadaa9530565
simonepollano03 <pollanosimone02@gmail.com>
2025-03-14 17:13:43 +0000

    Inicio Esqueleto

--- BackEnd/config.py
+++
@@ -1,5 +0,0 @@
-class Config:
-    SECRET_KEY = "llavesecretaDropHive"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///../BaseDatos/DropHive.db"  # Ubicación en la carpeta BaseDatos
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = "jwtsecretkey"
--- BackEnd/routes/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-from .auth_routes import auth_bp
-from .item_routes import item_bp
-from .category_routes import category_bp
-
-def register_routes(app):
-    app.register_blueprint(auth_bp, url_prefix="/auth")
-    app.register_blueprint(item_bp, url_prefix="/items")
-    app.register_blueprint(category_bp, url_prefix="/categories")
