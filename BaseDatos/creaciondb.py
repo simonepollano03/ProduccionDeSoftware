4ba5d5a55a20d442e1462f1d4f852543c6a69d59
simonepollano03 <pollanosimone02@gmail.com>
2025-03-14 18:14:43 +0000

    Modificación nombre para claridad

--- BaseDatos/creaciondb.py
+++ BaseDatos/creaciondb.py
@@ -1,7 +1,7 @@
 import sqlite3 as sql
 import os
 
-DB_PATH = os.path.abspath(os.path.dirname(__file__))+'\DropHive.db'
+DB_PATH = os.path.abspath(os.path.dirname(__file__)) + '/DropHive.db'
 
 def createDB():
     conn = sql.connect(DB_PATH)
@@ -17,10 +17,11 @@ def createDB():
                       name TEXT NOT NULL, 
                       description TEXT,
                       precio FLOAT,
-                      category_id INTEGER NOT NULL REFERENCES categorias,
+                      category_id INTEGER NOT NULL,
                       descuento FLOAT,
                       size TEXT,
-                      quantity INTEGER);''')
+                      quantity INTEGER,
+                      FOREIGN KEY (category_id) REFERENCES categorias(id));''')
     
     cursor.execute('''CREATE TABLE IF NOT EXISTS categorias
                      (id INTEGER PRIMARY KEY AUTOINCREMENT, 
@@ -28,20 +29,21 @@ def createDB():
                       descripcion TEXT);''')
     
     cursor.execute('''CREATE TABLE IF NOT EXISTS cuentas
-                   (id INTEGER PRIMARY KEY AUTOINCREMENT,
-                   nombre TEXT NOT NULL,
-                   correo TEXT NOT NULL unique,
-                   contraseña_hash TEXT NOT NULL,
-                   tlf TEXT,
-                   descripcion TEXT,
-                   direccion TEXT,
-                   privilegio INTEGER REFERENCES PRIVILEGIO);''')
+                     (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                      nombre TEXT NOT NULL,
+                      correo TEXT NOT NULL unique,
+                      contraseña_hash TEXT NOT NULL,
+                      tlf TEXT,
+                      descripcion TEXT,
+                      direccion TEXT,
+                      privilegio_id INTEGER,
+                      FOREIGN KEY (privilegio_od) REFERENCES privilegios(id));''')
     
     cursor.execute('''CREATE TABLE IF NOT EXISTS privilegios
                      (id INTEGER PRIMARY KEY AUTOINCREMENT, 
                       nombre TEXT NOT NULL UNIQUE,
                       permisos TEXT);''')
-    
+
     conn.commit()
     conn.close()
 
@@ -50,33 +52,32 @@ def addValues():
     cursor = conn.cursor()
 
     productos = [
-        (1, "Camiseta", "Camiseta de manga larga", 19.99, 1, 0, "S,M,L,XL", 100),
-        (2, "Pantalón", "Pantalón de cuero", 29.99, 1, 0, "M,L", 50),
+        ("Camiseta", "Camiseta de manga larga", 19.99, 1, 0, "S,M,L,XL", 100),
+        ("Pantalón", "Pantalón de cuero", 29.99, 1, 0, "M,L", 50),
     ]
 
     categorias = [
-        (1, "Ropa", "Categoría de ropa"),
-        (2, "Accesorios", "Categoría de accesorios"),
-        (3, "Limpieza", "Categoría de limpieza")
+        ("Ropa", "Categoría de ropa"),
+        ("Accesorios", "Categoría de accesorios"),
+        ("Limpieza", "Categoría de limpieza")
     ]
 
-    cuenta = [
-        (1, "Administrador", "admin@example.com", "pbkdf2:sha256:150000$j3i32R78$528663f42787f02477271a493d7f53d1546925197b22882b6458944a8f117667", "+573161234567", "Administrador", "Calle 123, 456", 1),
-        (2, "Usuario", "user@example.com", "pbkdf2:sha256:150000$j3i32R78$528663f42787f02477271a493d7f53d1546925197b22882b6458944a8f11", "+573161234567", "Administrador", "Calle 123, 456", 1)
+    cuentas = [
+        ("Administrador", "admin@example.com", "pbkdf2:sha256:150000$j3i32R78$528663f42787f02477271a493d7f53d1546925197b22882b6458944a8f117667", "+573161234567", "Administrador", "Calle 123, 456", 1),
+        ("Usuario", "user@example.com", "pbkdf2:sha256:150000$j3i32R78$528663f42787f02477271a493d7f53d1546925197b22882b6458944a8f11", "+573161234567", "Administrador", "Calle 123, 456", 1)
     ]
 
-    permisos = [
-        (1, "Administrador", "Administrador del sistema"),
-        (2, "Usuario", "Usuario normal"),
-        (3, "Visitante", "Visitante del sistema")
+    privilegios = [
+        ("Administrador", "Administrador del sistema"),
+        ("Usuario", "Usuario normal"),
+        ("Visitante", "Visitante del sistema")
     ]
 
-    conn.executemany("""INSERT INTO productos VALUES (?, ?, ?, ?, ?, ?, ?, ?)""", productos)
-    conn.executemany("""INSERT INTO categorias VALUES (?, ?, ?)""", categorias)
-    conn.executemany("""INSERT INTO cuentas VALUES (?, ?, ?, ?, ?, ?, ?, ?)""", cuenta)
-    conn.executemany("""INSERT INTO privilegios VALUES (?, ?, ?)""", permisos)
-
-
+    # Insertar valores
+    cursor.executemany("""INSERT INTO productos (name, description, precio, category_id, descuento, size, quantity) VALUES (?, ?, ?, ?, ?, ?, ?)""", productos)
+    cursor.executemany("""INSERT INTO categorias (name, descripcion) VALUES (?, ?)""", categorias)
+    cursor.executemany("""INSERT INTO cuentas (nombre, correo, contraseña_hash, tlf, descripcion, direccion, privilegio) VALUES (?, ?, ?, ?, ?, ?, ?)""", cuentas)
+    cursor.executemany("""INSERT INTO privilegios (nombre, permisos) VALUES (?, ?)""", privilegios)
 
     conn.commit()
     conn.close()
