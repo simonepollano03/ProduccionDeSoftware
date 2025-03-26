59e8a8ca09f9112d1b80cd4e357e4b4ab576d3b6
simonepollano03 <pollanosimone02@gmail.com>
2025-03-26 16:57:48 +0000

    Cambio para inicio de sesion

--- BackEnd/routes/auth.py
+++ BackEnd/routes/auth.py
@@ -24,17 +24,18 @@ def get_user_session(db_path):
 def login():
     data = request.get_json()
     username = data.get("username")
+    mail = data.get("mail")
     password = data.get("password")
 
     DB_PATH = get_db_path(username)
     db_session = get_user_session(DB_PATH)
 
     try:
-        account = db_session.query(Account).filter_by(name=username, password=password).first()
+        account = db_session.query(Account).filter_by(mail=mail, password=password).first()
         if account:
             session["user"] = account.name
             session["db_path"] = DB_PATH
-            return jsonify({"message": f"Bienvenido, {account.name}"}), 200
+            return jsonify({"message": f"Bienvenido, {account.name}", "mail": account.mail}), 200
         else:
             return jsonify({"error": "Credenciales incorrectas"}), 401
     finally:
