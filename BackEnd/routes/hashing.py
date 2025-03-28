52d0c4eb5cd1fbf987a8cecd6f873800c0d649a9
simonepollano03 <pollanosimone02@gmail.com>
2025-03-28 20:03:44 +0000

    Ahora la contraseña se codifica para guardarse en la base de datos

--- BackEnd/routes/hashing.py
+++ BackEnd/routes/hashing.py
@@ -1,10 +1,12 @@
 import bcrypt
 
+
 def creacion_hash(password):
     # Generar un salt y luego hacer el hash de la contraseña
     salt = bcrypt.gensalt()
     hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed
+
 
 def verificar_hash(password, hashed_password):
     # Verificar que la contraseña coincida con el hash guardado
