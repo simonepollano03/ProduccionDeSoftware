a5a9a76c3796a7c7c3b33d4c469a68806390ea65
simonepollano03 <pollanosimone02@gmail.com>
2025-03-28 22:44:46 +0000

    Carga del header

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -1,6 +1,7 @@
 import os
 
-from flask import Flask, render_template, session, redirect, url_for
+from flask import Flask, render_template, session, redirect, url_for, send_file
+from werkzeug.utils import send_from_directory
 
 from BackEnd.routes.Accounts import accounts_bp
 from BackEnd.routes.Auth import auth_bp
@@ -8,6 +9,8 @@ from BackEnd.routes.Category import categories_bp
 from BackEnd.routes.Privilege import privileges_bp
 from BackEnd.routes.Product import products_bp
 from BackEnd.routes.Register import registro_bp
+
+from pathlib import Path
 
 app = Flask(__name__, template_folder="../FrontEnd/html", static_folder="../FrontEnd")
 app.secret_key = os.getenv("test", "1234")
@@ -32,6 +35,20 @@ def index():
 def home(db_name):
     return render_template("home.html")
 
+
+@app.route('/templates/<template_name>')
+def servir_header_parcial(template_name):
+    template = os.path.join(app.template_folder, template_name)
+    template2 = Path(template)
+
+    try:
+        # Verificación de seguridad básica
+        if not template_name.endswith('.html'):
+            template_name += '.html'
+
+        return send_file(template2)
+    except Exception as e:
+        return f"Error al cargar el template: {str(e)}", 404
 
 @app.route("/login")
 def login():
--- FrontEnd/JavaScript/header.js
+++ FrontEnd/JavaScript/header.js
@@ -1,6 +1,6 @@
 async function loadTemplate(templateName, targetElementId) {
     try {
-        const response = await fetch(`../html/${templateName}.html`); // Cambié la ruta
+        const response = await fetch(`http://127.0.0.1:4000/templates/${templateName}.html`); // Cambié la ruta
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
