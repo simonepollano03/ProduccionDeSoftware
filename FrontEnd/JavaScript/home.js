6aa81e4ec47ad4bb9685012118b69eace7cf502d
simonepollano03 <pollanosimone02@gmail.com>
2025-05-05 16:04:14 +0100

    Supply accesible desde home.

--- FrontEnd/JavaScript/home.js
+++ FrontEnd/JavaScript/home.js
@@ -45,7 +45,7 @@ function registrarEventos() {
 
 async function redirigirASupply() {
     const dbName = await recuperarNombreBaseDatos();
-    window.location.href = `http://127.0.0.1:4000/${dbName}/supply`;
+    window.location.href = `http://127.0.0.1:4000/supply`;
 }
 
 async function manejarCambioPagina() {
